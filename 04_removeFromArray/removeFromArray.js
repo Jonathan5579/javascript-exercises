e80b789a1e2ba31aa1fed955a5a0a9516e116e74
Jonathan Briones <jonathan.briones@siix-global.com>
2024-10-14 12:56:16 -0600

    04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,25 @@
 const removeFromArray = function() {
+   
+   let array = arguments[0];
+   let numbersToDelete = [];
+
+
+   for (let i = 1; i < arguments.length; i++) {
+      numbersToDelete.push(arguments[i]);
+   }
+   console.log(`ToDelete: ${numbersToDelete}, fromArray: ${array}`)
+
+   
+   let newArray = [];
+   
+   for (let i=0; i<array.length; i++){
+      if (numbersToDelete.includes(array[i])){
+         continue
+      }
+      newArray.push(array[i])
+   }
+   console.log(`ToDelete: ${numbersToDelete}, finalArray: ${newArray}`);
+   return newArray
 };
 
 // Do not edit below this line
