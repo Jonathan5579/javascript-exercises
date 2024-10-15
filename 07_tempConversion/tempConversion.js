c2bcbb7f781d99176bcff694c9a9e7502864a2b9
Jonathan Briones <jonathan.briones@siix-global.com>
2024-10-15 14:58:24 -0600

    07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheitDegrees) {
+  let num = (5/9)*(farenheitDegrees-32)
+  return Math.round(num*10)/10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusDegrees) {
+  let num = (9/5)*(celsiusDegrees)+32
+  return Math.round(num*10)/10
 };
 
 // Do not edit below this line
