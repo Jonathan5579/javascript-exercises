7f825d2c89b9f397c8be8e48d8c2a40a65df79a7
Jonathan Briones <jonathan.briones@siix-global.com>
2024-10-15 14:43:47 -0600

    06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,26 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+   /**Multiple if solution*/
+   if (year%4 == 0){
+      //possible leap year
+      if (year%100 == 0){
+         //not leap year unles...
+         if (year%400==0){
+            return true
+         }
+         return false
+      }
+      return true
+   }
+   return false
+
+   /*
+   //*One if solution
+   if (year%4==0 && (year%100!=0 || year%400==0)){
+      return true
+   }
+   return false
+   */
 };
 
 // Do not edit below this line
